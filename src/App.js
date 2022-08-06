a270772d2cb878dffa792a60511428ec631b3e0c
rnjswltn1020 <rnjswltn1020@naver.com>
2022-08-06 09:37:08 +0900

    recoil 비동기데이터 셋팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
+import { atom, useRecoilState } from "recoil";
+
+//상태 관리할 초기값 설정.
+const flickrState = atom({
+  key: "flickr",
+  default: [],
+});
+
 function App() {
-	return (
-		<h1>Hello</h1>
-	);
+  const [Pics, setPics] = useRecoilState(flickrState);
+  const callFlickr = async () => {
+    const key = "fefec6c874c53fb1655dae7c9e82ab25";
+    const method_search = "flickr.photos.search";
+    const url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${key}&per_page=10&nojsoncallback=1&format=json&tags=landscape`;
+
+    const res = await fetch(url);
+    const body = await res.json();
+    console.log(body.photos.photo);
+    setPics(body.photos.photo);
+  };
+  useEffect(() => {
+    callFlickr();
+  }, []);
+  return (
+    <div>
+      {Pics.map((pic) => {
+        return (
+          <article key={pic.id}>
+            <h2>{pic.title}</h2>
+          </article>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
